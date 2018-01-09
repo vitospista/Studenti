380631ad061ddaf11d15929baa59ec615b12498b
triprog-1 <triprog-1@TRILPROG-010>
2018-01-09 11:24:52 +0100

    Numbers

---
+++ CorsoEnaip2018_Numbers/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Globalization;
+
+namespace CorsoEnaip2018_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(FindMaxInt1());
+            //Console.WriteLine(FindMaxInt2());
+            //Console.WriteLine(FindMaxInt3());
+            //Floatings();
+            //Console.WriteLine(GetMaxFloat());
+
+            double d1 = 0.1;
+            double d2 = 0.2;
+            Console.WriteLine("La somma è 0.3? " + (d1+d2 == 0.3));
+
+            Console.Read();
+        }
+
+        private static int FindMaxInt1()
+        {
+            int i = 1;
+
+            while (i > 0)
+                i = i + 1;
+
+            int max = i - 1;
+
+            return max;
+        }
+
+        private static int FindMaxInt2()
+        {
+            int i = 0;
+
+            while (i + 1 > 0)
+                i = i + 1;
+
+            return i;
+        }
+
+        public static int FindMaxInt3()
+        {
+            return int.MaxValue;
+        }
+
+        public static void Floatings()
+        {
+            int iii = 5;
+
+            checked
+            {
+                long l = 123456123123;
+
+                //questo non si può fare
+                //int i = l;
+
+                //cast esplicito
+                //senza il checked viene fuori un intero inconsistente
+                //int i = (int)l;
+
+                //Console.WriteLine(i);
+            }
+
+            int ii = 123456;
+
+            //cast implicito
+            long ll = ii;
+
+            // con marcatore
+            float f = 3.5f;
+
+            //con cast esplicito
+            float ff = (float)3.5;
+
+            double d = f;
+
+            double.TryParse(
+                "   7,123.456  ",
+                NumberStyles.AllowThousands
+                    | NumberStyles.AllowDecimalPoint
+                    | NumberStyles.AllowLeadingWhite
+                    | NumberStyles.AllowTrailingWhite,
+                CultureInfo.GetCultureInfo("en-US"),
+                out d);
+            Console.WriteLine(d);
+        }
+
+        private static float GetMaxFloat()
+        {
+            float f = 1;
+
+            while (f > 0)
+                f = f + 1000;
+                //f++;
+
+            return f - 1;
+        }
+    }
+}
---
+++ CorsoEnaipNet2018_IntCalculator/Program.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorsoEnaipNet2018_IntCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //HardCodedNumbers();
+            //NumbersFromConsole_BadCode();
+            //NumbersFromConsole_GoodCode();
+            IntCalculator();
+
+            Console.Read();
+        }
+
+        private static void IntCalculator()
+        {
+            Console.WriteLine("*** Calcolatrice ***");
+
+            string mustContinue;
+
+            do
+            {
+                int number1 = ReadIntSafe();
+
+                string operation = ReadOperationSafe();
+
+                int number2 = ReadIntSafe();
+
+                int result = ExecuteOperation(number1, operation, number2);
+
+                Console.WriteLine("risultato: " + result);
+
+                Console.WriteLine("Vuoi continuare (s/n)? ");
+
+                mustContinue = Console.ReadLine();
+
+            } while (mustContinue == "s");
+        }
+
+        private static int ExecuteOperation(int number1, string operation, int number2)
+        {
+            switch(operation)
+            {
+                case "+":
+                    return number1 + number2;
+                case "-":
+                    return number1 - number2;
+                case "*":
+                    return number1 * number2;
+                case "/":
+                    return number1 / number2;
+                default:
+                    throw new InvalidOperationException();
+            }
+        }
+
+        private static string ReadOperationSafe()
+        {
+            Console.Write("Inserisci l'operazione da eseguire: ");
+
+            string operation = Console.ReadLine();
+
+            // un po' troppo verboso
+            //while(operation != "+" &&
+            //    operation != "-" &&
+            //    operation != "*" &&
+            //    operation != "/")
+            //{
+            //    Console.Write("Non hai inserito un'operazione valida! Riprova: ");
+            //    operation = Console.ReadLine();
+            //}
+
+            List<string> availableOperations = new List<string>
+            {
+                "+", "-", "*", "/"
+            };
+
+            while(!availableOperations.Contains(operation))
+            {
+                Console.Write("Non hai inserito un'operazione valida! Riprova: ");
+                operation = Console.ReadLine();
+            }
+
+            return operation;
+        }
+
+        private static void NumbersFromConsole_BadCode()
+        {
+            string read = Console.ReadLine();
+
+            int number;
+            int secondNumber;
+
+            bool succeeded = int.TryParse(read, out number);
+
+            if (succeeded)
+            {
+                string secondRead = Console.ReadLine();
+                bool secondSucceeded = int.TryParse(secondRead, out secondNumber);
+            }
+            else
+            {
+                Console.WriteLine("Il valore inserito non è un numero! Riprova: ");
+                read = Console.ReadLine();
+                succeeded = int.TryParse(read, out number);
+
+                string secondRead = Console.ReadLine();
+                bool secondSucceeded = int.TryParse(secondRead, out secondNumber);
+            }
+
+            Console.WriteLine("Somma: " + (number + secondNumber));
+        }
+
+        private static void NumbersFromConsole_GoodCode()
+        {
+            Console.WriteLine("Questo programma calcola la somma di due numeri interi");
+
+            int number1 = ReadIntSafe();
+            int number2 = ReadIntSafe();
+
+            Console.WriteLine("Somma: " + (number1 + number2));
+        }
+
+        private static int ReadIntSafe()
+        {
+            //Qui ci sono ancora duplicazioni:
+
+            //Console.WriteLine("Inserisci un numero: ");
+            //string read = Console.ReadLine();
+            //int number;
+            //bool succeeded = int.TryParse(read, out number);
+            //while (!succeeded)
+            //{
+            //    Console.WriteLine("Il valore inserito non è un numero! Riprova: ");
+            //    read = Console.ReadLine();
+            //    succeeded = int.TryParse(read, out number);
+            //}
+            //return number;
+
+            Console.Write("Inserisci un numero: ");
+
+            int number;
+
+            while(!int.TryParse(Console.ReadLine(), out number))
+            {
+                Console.Write("Valore non valido! Riprova: ");
+            }
+
+            return number;
+        }
+
+        private static void HardCodedNumbers()
+        {
+            //dichiarazione
+            int oreTeoria;
+            //assegnazione
+            oreTeoria = 194;
+
+            //dichiarazione e assegnazione insieme
+            int orePratica = 136;
+
+            Console.WriteLine("Ore totali all'ENAIP: " + (oreTeoria + orePratica));
+        }
+    }
+}
