d2d18ccd8a6ba2fc74608bf6acd1de9f5a5924e3
triprog-1 <triprog-1@TRILPROG-010>
2018-01-15 11:03:55 +0100

    Esercizio su ExtensibleArray, con istruzioni per implementare il Remove

---
+++ CorsoEnaip2018_MyList/ExtensibleArray.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorsoEnaip2018_MyList
+{
+    /// <summary>
+    /// Array per aggiungere e togliere elementi
+    /// </summary>
+    public class ExtensibleArray
+    {
+        public int[] _array;
+        public int _index;
+
+        public ExtensibleArray(int size)
+        {
+            //if (size <= 0)
+            //    throw new ArgumentException("size cannot be 0 or negative!");
+
+            //risolvere size iniziale a 0: modo 1
+            //if (size == 0)
+            //    this._array = new int[1];
+            //else
+            //    this._array = new int[size];
+
+            this._array = new int[size];
+
+            // questo non è necessario,
+            // perché quando un'istanza è creata,
+            // tutti i campi vengono inizializzati
+            // con i loro valori di default
+            this._index = 0;
+        }
+
+        public void Add(int i)
+        {
+            if (this._array.Length <= this._index)
+            {
+                //risolvere size iniziale a 0: modo 2
+
+                //int newLength = this._array.Length * 2;
+
+                //if (newLength == 0)
+                //    newLength = 1;
+
+                //int[] newArray = new int[newLength];
+
+                //risolvere size iniziale a 0: modo 3
+                int[] newArray = new int[Math.Max(1, this._array.Length * 2)];
+
+                for(int j = 0; j < this._array.Length; j++)
+                {
+                    newArray[j] = this._array[j];
+                }
+
+                this._array = newArray;
+            }
+
+            this._array[this._index++] = i;
+        }
+
+        public void Remove(int i)
+        {
+            /*
+             * esercizio: rimuove tutte le occorrenze dell'intero i dall'array.
+             * 
+             * es: parto da [5 4 7 5 2], quindi
+             * l'array interno è [5 4 7 5 2 0 0 0], con lunghezza 8;
+             * chiamo Remove(5)
+             *     1) compatto l'array interno, rimuovendo tutti i 5
+             *         [4 7 2 0 0 0 0 0]
+             *     2) se l'insieme di elementi che rimangono
+             *         è meno della metà della lunghezza dell'array,
+             *         devo creare un nuovo array grande la metà,
+             *         e trasferire i valori (un po' come avevo fatto
+             *         per l'Add, ma invece di creare un array doppio
+             *         qui devo creare un array grande la metà.
+             *        4 7 2 0
+             */
+
+            throw new NotImplementedException();
+        }
+
+        public int Count()
+        {
+            return this._index;
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                if (!(index < this._index))
+                    throw new IndexOutOfRangeException();
+
+                return this._array[index];
+
+                // modi alternativi per ottenere lo STESSO comportamento
+
+                //if (!(index < this._index))
+                //    throw new IndexOutOfRangeException();
+                //else
+                //    return this._array[index];
+
+                //if (!(index < this._index))
+                //{
+                //    throw new IndexOutOfRangeException();
+                //}
+                //else
+                //{
+                //    return this._array[index];
+                //}
+            }
+            set
+            {
+                if (!(index < this._index))
+                    throw new IndexOutOfRangeException();
+
+                this._array[index] = value;
+            }
+        }
+    }
+}
--- CorsoEnaip2018_MyListClient/Program.cs
+++ CorsoEnaip2018_MyListClient/Program.cs
@@ -11,9 +11,79 @@ namespace CorsoEnaip2018_MyListClient
     {
         static void Main(string[] args)
         {
-            MyList l = new MyList(10);
+            ExtensibleArray a1 = new ExtensibleArray(4);
 
-            l[4] = 12;
+            a1.Add(45);
+            Console.WriteLine($"a1[0] = {a1[0]}");
+            Console.WriteLine($"a1.Count() = {a1.Count()}");
+
+            a1.Add(23);
+
+            a1.Add(123);
+            Console.WriteLine($"a1[2] = {a1[2]}");
+            Console.WriteLine($"a1.Count() = {a1.Count()}");
+
+            a1.Add(65);
+
+            a1.Add(123123);
+            Console.WriteLine($"a1[4] = {a1[4]}");
+            Console.WriteLine($"a1.Count() = {a1.Count()}");
+
+            for (int i = 0; i < 1000; i++)
+                a1.Add(i);
+
+            Console.WriteLine($"a1[1004] = {a1[1004]}");
+            Console.WriteLine($"a1.Count() = {a1.Count()}");
+
+            try
+            {
+                ExtensibleArray a2 = new ExtensibleArray(-1);
+                Console.WriteLine($"a2.Count() = {a2.Count()}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("size negativo lancia eccezione!");
+                Console.WriteLine(ex.Message);
+            }
+
+            ExtensibleArray a3 = new ExtensibleArray(0);
+
+            a3.Add(45);
+            Console.WriteLine($"a3[0] = {a3[0]}");
+            Console.WriteLine($"a3.Count() = {a3.Count()}");
+
+            a3.Add(23);
+
+            a3.Add(123);
+            Console.WriteLine($"a3[2] = {a3[2]}");
+            Console.WriteLine($"a3.Count() = {a3.Count()}");
+
+            a3.Add(432);
+            a3.Add(987);
+
+            try
+            {
+                Console.WriteLine(a3[5]);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("accedere a un indice > _index lancia eccezione!");
+                Console.WriteLine(ex.Message);
+            }
+
+            try
+            {
+                a3[5] = 765765;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("settare un valore in indice > _index lancia eccezione!");
+                Console.WriteLine(ex.Message);
+            }
+
+
+
+            Console.Read();
         }
     }
 }
