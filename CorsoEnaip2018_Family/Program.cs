78bbc3af8a593e33dbf0cd4c732ebaf7e05ea636
triprog-1 <triprog-1@TRILPROG-010>
2018-01-23 13:08:36 +0100

    Family con interfacce, preparazione provetta

---
+++ CorsoEnaip2018_Family/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorsoEnaip2018_Family
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mum = new Mum("Leila");
+            var dad = new Dad("Han Solo");
+            mum.MakeBaby(dad, "Kylo Ren");
+            mum.Child.StartCrying();
+
+            Console.Read();
+        }
+    }
+
+    class Person
+    {
+        public string Name { get; set; }
+
+        public Person(string name)
+        {
+            Name = name;
+        }
+    }
+
+    class Dad : Person, IComforter
+    {
+        public Dad(string name)
+            : base(name)
+        { }
+
+        public void Comfort(Baby b)
+        {
+            Console.WriteLine($"Dad {Name} gives a caress to the Baby {b.Name}");
+        }
+
+        public void EscapeToMexico(Baby b)
+        {
+            // ? how can I do this?
+        }
+
+    }
+
+    class Mum : Person, IComforter
+    {
+        public Baby Child { get; private set; }
+
+        public Mum(string name)
+            : base(name)
+        { }
+
+        public void MakeBaby(Dad d, string babyName)
+        {
+            Console.WriteLine($"Mum {Name} made a Baby with Dad {d.Name}");
+            Child = new Baby(babyName);
+            Child.AddComforter(this);
+            Child.AddComforter(d);
+        }
+
+        public void Comfort(Baby b)
+        {
+            Console.WriteLine($"Mum {Name} comforts Baby {b.Name}");
+        }
+    }
+
+    class Baby : Person
+    {
+        private List<IComforter> _comforters;
+
+        public Baby(string name)
+            : base(name)
+        {
+            _comforters = new List<IComforter>();
+        }
+
+        public void AddComforter(IComforter c)
+        {
+            _comforters.Add(c);
+        }
+
+        public void StartCrying()
+        {
+            Console.WriteLine($"Baby {Name} started crying.");
+
+            foreach (var c in _comforters)
+                c.Comfort(this);
+        }
+    }
+
+    interface IComforter
+    {
+        void Comfort(Baby b);
+    }
+}
--- CorsoEnaip2018_NewsFeed/Program.cs
+++ CorsoEnaip2018_NewsFeed/Program.cs
@@ -1,16 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CorsoEnaip2018_NewsFeed
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            /*
+        // Design Pattern Publisher - Subscriber
+
+        /*
              * Ho più Server che pubblicano notizie.
              * Ho una lista di Computer che possono ascoltare l'arrivo delle notizie
              * dai diversi Server.
@@ -19,7 +16,8 @@ namespace CorsoEnaip2018_NewsFeed
              * 
              * Il Server deve implementare un'interfaccia IPublisher,
              *    che ha un metodo "void Publish(string news)"
-             *    e un metodo "void AddSubscriber(ISubscriber subscriber)".
+             *    e un metodo "void AddSubscriber(ISubscriber subscriber)",
+             *    e un metodo "void RemoveSubscriber(ISubscriber subscriber)"
              * In questo metodo il Server passa a tutti i suoi Subscribers la notizia.
              * Il Computer deve implementare un'interfaccia ISubscriber,
              *     con un metodo "void Receive(string server, string news)"
@@ -41,6 +39,111 @@ namespace CorsoEnaip2018_NewsFeed
              * "c2 ha ricevuto notizia da s1: "3a Guerra Mondiale""
              * 
              */
+        static void Main(string[] args)
+        {
+            var c1 = new Computer("c1");
+            var c2 = new Computer("c2");
+
+            var s1 = new Server("s1");
+            var s2 = new Server("s2");
+
+            Console.WriteLine("Chiamo s1 senza subscribers:");
+            s1.Publish("news1");
+
+            Console.WriteLine("Sottoscrivo c1 a s1");
+            c1.Subscribe(s1);
+            s1.Publish("news2");
+
+            Console.WriteLine("Disiscrivo c1 a s1");
+            c1.Unsubscribe(s1);
+            s1.Publish("news3");
+
+            Console.WriteLine("Sottoscrivo c1 a s1 e s2");
+            c1.Subscribe(s1);
+            c1.Subscribe(s2);
+            s1.Publish("news4");
+            s2.Publish("news5");
+
+            Console.WriteLine("Sottoscrivo c2 a s1 e s2");
+            c2.Subscribe(s1);
+            c2.Subscribe(s2);
+            s1.Publish("news6");
+            s2.Publish("news7");
+
+            Console.WriteLine("Disiscrivo c1 da s2");
+            c1.Unsubscribe(s2);
+            s1.Publish("news8");
+            s2.Publish("news9");
+
+            Console.Read();
+        }
+    }
+
+    interface IPublisher
+    {
+        void AddSubscriber(ISubscriber subscriber);
+        void RemoveSubscriber(ISubscriber subscriber);
+        void Publish(string news);
+    }
+
+    interface ISubscriber
+    {
+        void Subscribe(IPublisher publisher);
+        void Unsubscribe(IPublisher publisher);
+        void Receive(string server, string news);
+    }
+
+    class Server : IPublisher
+    {
+        private List<ISubscriber> _subscribers;
+
+        public string Name { get; }
+
+        public Server(string name)
+        {
+            Name = name;
+            _subscribers = new List<ISubscriber>();
+        }
+
+        public void AddSubscriber(ISubscriber subscriber)
+        {
+            _subscribers.Add(subscriber);
+        }
+
+        public void RemoveSubscriber(ISubscriber subscriber)
+        {
+            _subscribers.Remove(subscriber);
+        }
+
+        public void Publish(string news)
+        {
+            foreach (var sub in _subscribers)
+                sub.Receive(Name, news);
+        }
+    }
+
+    class Computer : ISubscriber
+    {
+        public Computer(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; }
+
+        public void Receive(string server, string news)
+        {
+            Console.WriteLine($"Computer {Name} received from server {server} the news: '{news}'");
+        }
+
+        public void Subscribe(IPublisher publisher)
+        {
+            publisher.AddSubscriber(this);
+        }
+
+        public void Unsubscribe(IPublisher publisher)
+        {
+            publisher.RemoveSubscriber(this);
         }
     }
 }
