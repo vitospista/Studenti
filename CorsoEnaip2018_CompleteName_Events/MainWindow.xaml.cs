e7ab96e7153211be9974ee4d7c69786f3e72f45d
triprog-1 <triprog-1@TRILPROG-010>
2018-01-24 12:09:25 +0100

    Esercizio sugli eventi

---
+++ CorsoEnaip2018_CompleteName_Events/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CorsoEnaip2018_CompleteName_Events
+{
+    public partial class MainWindow : Window
+    {
+        /*
+         * Quando l'utente cambia Name o Surname,
+         * La Label CompleteName deve aggiornarsi concatenandoli,
+         * e la Label CharsCount deve aggiornarsi
+         *     mostrando il conto totale dei caratteri.
+         * 
+         * Usare l'evento TextChanged delle TextBox.
+         * 
+         * CompleteNameLabel.Content = "ciao";
+         * 
+         */
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            NameTextBox.TextChanged += WriteCompleteName;
+            SurnameTextBox.TextChanged += WriteCompleteName;
+        }
+
+        public void WriteCompleteName(object sender, TextChangedEventArgs e)
+        {
+            var completeName = NameTextBox.Text + " " + SurnameTextBox.Text;
+
+            CompleteNameLabel.Content = completeName;
+            CharsCountLabel.Content = completeName.Length;
+        }
+    }
+}
--- CorsoEnaip2018_NewsFeed_Events/Program.cs
+++ CorsoEnaip2018_NewsFeed_Events/Program.cs
@@ -18,6 +18,87 @@ namespace CorsoEnaip2018_NewsFeed_Events
 
         static void Main(string[] args)
         {
+            var c1 = new Computer { Name = "c1" };
+            var c2 = new Computer { Name = "c2" };
+
+            var s1 = new Server { Name = "s1" };
+            var s2 = new Server { Name = "s2" };
+
+            Console.WriteLine("Chiamo s1 senza subscribers:");
+            s1.Publish("news1");
+
+            Console.WriteLine("Sottoscrivo c1 a s1");
+            s1.Published += c1.Receive;
+            s1.Publish("news2");
+
+            Console.WriteLine("Disiscrivo c1 a s1");
+            s1.Published -= c1.Receive;
+            s1.Publish("news3");
+
+            Console.WriteLine("Sottoscrivo c1 a s1 e s2");
+            s1.Published += c1.Receive;
+            s2.Published += c1.Receive;
+            
+            s1.Publish("news4");
+            s2.Publish("news5");
+
+            Console.WriteLine("Sottoscrivo c2 a s1 e s2");
+            s1.Published += c2.Receive;
+            s2.Published += c2.Receive;
+            s1.Publish("news6");
+            s2.Publish("news7");
+
+            Console.WriteLine("Disiscrivo c1 da s2");
+            s2.Published -= c1.Receive;
+            s1.Publish("news8");
+            s2.Publish("news9");
+
+            Console.Read();
+        }
+    }
+
+    class Server
+    {
+        public event PublishHandler Published;
+
+        public void Publish(string news)
+        {
+            if (Published != null)
+            {
+                Published.Invoke(this, news);
+            }
+
+            // In C# 6.0 posso usare l'operatore Elvis sui metodi.
+            // per fare i check sui null:
+            // Published?.Invoke(this, news);
+        }
+
+        public string Name { get; set; }
+    }
+
+    delegate void PublishHandler(Server s, string news);
+
+    class Computer
+    {
+        public string Name { get; set; }
+
+        public void Receive(Server s, string news)
+        {
+            Console.WriteLine(
+                $"{Name} received from server {s.Name} the following news: '{news}'");
+        }
+
+        public void Reject(Server s, string news)
+        {
+            // posso agganciare anche questo metodo,
+            // perché rispetta la firma del delegate PublishHandler.
+        }
+
+        public void Receive(string news)
+        {
+            // Questo metodo NON è agganciabile all'evento Published del server,
+            // perché NON rispetta la firma del delegate:
+            // manca il parametro Server.
         }
     }
 }
