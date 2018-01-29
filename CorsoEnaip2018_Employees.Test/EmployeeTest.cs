c3fd28561e81c3bd2763daf731aa4b78157b9cea
triprog-1 <triprog-1@TRILPROG-010>
2018-01-29 12:59:33 +0100

    Employees project; esercizio sugli scacchi

---
+++ CorsoEnaip2018_Employees.Test/EmployeeTest.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CorsoEnaip2018_Employees;
+
+namespace CorsoEnaip2018_Employees.Test
+{
+    [TestClass]
+    public class EmployeeTest
+    {
+        [TestMethod]
+        public void Employee_AddSalary_cumulates_salaries()
+        {
+            var e = new FixedSalaryEmployee();
+
+            Assert.AreEqual(0, e.TotalPay);
+
+            e.AddSalary(1000);
+            Assert.AreEqual(1000, e.TotalPay);
+
+            e.AddSalary(2000);
+            e.AddSalary(2000);
+            Assert.AreEqual(5000, e.TotalPay);
+        }
+
+        [TestMethod]
+        public void FixedSalaryEmployee_CalculatePay_is_correct()
+        {
+            var e = new FixedSalaryEmployee();
+
+            e.MonthlySalary = 1500;
+
+            var pay = e.CalculatePay(2018, 1);
+
+            Assert.AreEqual(1500, pay);
+        }
+
+        [TestMethod]
+        public void HourlyEmployee_CalculatePay_is_correct()
+        {
+            var e = new HourlyEmployee();
+            e.HourlySalary = 20;
+            Assert.AreEqual(0, e.CalculatePay(2018, 1));
+
+            e.AddWorkedHours(new DateTime(2018, 1, 29), 4);
+            Assert.AreEqual(80, e.CalculatePay(2018, 1));
+
+            e.AddWorkedHours(new DateTime(2018, 1, 30), 10);
+            Assert.AreEqual(300, e.CalculatePay(2018, 1), "only January hours");
+
+            e.AddWorkedHours(new DateTime(2017, 12, 23), 6);
+            Assert.AreEqual(300, e.CalculatePay(2018, 1), "added non-January hours");
+
+            e.AddWorkedHours(new DateTime(2018, 1, 29), 3);
+            Assert.AreEqual(360, e.CalculatePay(2018, 1));
+            /*
+             * 20 € / ora
+             * 
+             * 29 Jan
+             * 4 ore => 4 * 20 = 80
+             * 
+             * 30 Jan
+             * 10 ore => 8 * 20 + 2 * 30 = 160 + 60 = 220
+             * 
+             * Total Jan = 300
+             * 
+             */
+
+        }
+
+        [TestMethod]
+        public void CommissionEmployee_CalculatePay_is_correct()
+        {
+            var e = new CommissionEmployee();
+            e.CommissionPercentage = (decimal)0.02;
+            Assert.AreEqual(0, e.CalculatePay(2018, 1));
+
+            e.AddCommission(new DateTime(2018, 1, 2), 40000); // 800
+            e.AddCommission(new DateTime(2018, 1, 15), 5000); // 100
+            Assert.AreEqual(900, e.CalculatePay(2018, 1));
+
+            e.AddCommission(new DateTime(2018, 2, 15), 10000);
+            Assert.AreEqual(900, e.CalculatePay(2018, 1));
+
+            e.AddCommission(new DateTime(2018, 1, 15), 10000); // 200
+            Assert.AreEqual(1100, e.CalculatePay(2018, 1));
+        }
+
+        // TODO: approfondire Equals
+        //[TestMethod]
+        //public void CheckC()
+        //{
+        //    var c1 = new C { Id = 1 };
+        //    var c2 = new C { Id = 1 };
+
+        //    Assert.AreEqual(c1, c2);
+        //    Assert.IsTrue((c1 == null && c2 == null) || c1.Equals(c2));
+        //    Assert.IsTrue(c1 == c2);
+
+        //}
+
+        //class C
+        //{
+        //    public int Id { get; set; }
+
+        //    public override bool Equals(object obj)
+        //    {
+        //        var other = obj as C;
+
+        //        if (other == null)
+        //            return false;
+
+        //        return Id == other.Id;
+        //    }
+
+        //    public override int GetHashCode()
+        //    {
+        //        return Id;
+        //    }
+        //}
+    }
+}
---
+++ CorsoEnaip2018_Employees/Employees.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorsoEnaip2018_Employees
+{
+    /* devo gestire diversi tipi di impiegati:
+    * 1) dipendenti diretti: paga fissa mensile
+    * 2) dipendenti a ore: tot paga all'ora;
+    *     50% in più per le ore di straordinario (+ di 8 ore).
+    * 3) dipendenti per commessa: pagati solo sulle commesse che vendono
+    *     (prendono una percentuale).
+    * 
+    * Ogni ultimo del mese il sistema Payroll entra in gioco,
+    * itera sulla lista di Employee, per ognuno calcola la paga,
+    * e gliela aggiunge al conto.
+    * 
+    * Ogni Employee ha un metodo CalculatePay, e una proprietà TotalPay.
+    */
+
+    public abstract class Employee
+    {
+        public decimal TotalPay { get; private set; }
+
+        public void AddSalary(decimal salary)
+        {
+            TotalPay += salary;
+        }
+
+        public abstract decimal CalculatePay(int year, int month);
+    }
+
+    public class FixedSalaryEmployee : Employee
+    {
+        public decimal MonthlySalary { get; set; }
+
+        public override decimal CalculatePay(int year, int month)
+        {
+            return MonthlySalary;
+        }
+    }
+
+    public class HourlyEmployee : Employee
+    {
+        private Dictionary<DateTime, int> _hours;
+
+        public HourlyEmployee()
+        {
+            _hours = new Dictionary<DateTime, int>();
+        }
+
+        public void AddWorkedHours(DateTime day, int hours)
+        {
+            if (_hours.ContainsKey(day))
+            {
+                _hours[day] += hours;
+                // è esattamente equivalente a:
+                //_hours[day] = _hours[day] + hours;
+            }
+            else
+            {
+                _hours.Add(day, hours);
+            }
+        }
+
+        public override decimal CalculatePay(int year, int month)
+        {
+            decimal totalMonthlySalary = 0;
+
+            var filteredHours = _hours
+                .Where(x => x.Key.Year == year && x.Key.Month == month);
+
+            foreach (var h in filteredHours)
+                totalMonthlySalary += amoutForHours(h.Value);
+
+            return totalMonthlySalary;
+        }
+
+        private decimal amoutForHours(int h)
+        {
+            var extraordinaries = h - 8;
+
+            if (extraordinaries > 0)
+            {
+                return 8 * HourlySalary + extraordinaries * HourlySalary * (decimal)1.5;
+            }
+            else
+            {
+                return h * HourlySalary;
+            }
+        }
+
+        public decimal HourlySalary { get; set; }
+    }
+
+    public class CommissionEmployee : Employee
+    {
+        private Dictionary<DateTime, decimal> _commissions;
+
+        public CommissionEmployee()
+        {
+            _commissions = new Dictionary<DateTime, decimal>();
+        }
+
+        public decimal CommissionPercentage { get; set; }
+
+        public override decimal CalculatePay(int year, int month)
+        {
+            return _commissions
+                .Where(x => x.Key.Year == year && x.Key.Month == month)
+                .Sum(x => x.Value) * CommissionPercentage;
+        }
+
+        public void AddCommission(DateTime day, decimal amount)
+        {
+            if (_commissions.ContainsKey(day))
+            {
+                _commissions[day] += amount;
+            }
+            else
+            {
+                _commissions.Add(day, amount);
+            }
+        }
+    }
+}
