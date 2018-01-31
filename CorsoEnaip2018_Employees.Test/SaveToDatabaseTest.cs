b77f9543b7fc90a15efda70ff064e9ec4714b586
triprog-1 <triprog-1@TRILPROG-010>
2018-01-31 09:42:15 +0100

    Employee - save to database - testo esercizio

---
+++ CorsoEnaip2018_Employees.Test/SaveToDatabaseTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorsoEnaip2018_Employees.Test
+{
+    [TestClass]
+    class SaveToDatabaseTest
+    {
+        [TestMethod]
+        public void SaveAndGet()
+        {
+            // creo lista di Employee con Pay/Bonus/Malus diversi.
+
+            // salvo la lista di Employee su una tabella del database.
+
+            // recupero dal database la lista.
+
+            // verifico che la nuova lista ha gli stessi valori
+            // della lista di partenza.
+        }
+    }
+}
