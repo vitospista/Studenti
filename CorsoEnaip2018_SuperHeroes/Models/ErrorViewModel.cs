8ad5e1a1feb76d4aebd731641c861e5779d4f797
Massimiliano Kraus <massimiliano.kraus@gmail.com>
2018-03-09 12:53:00 +0100

    MVC - template completo, middleware

---
+++ CorsoEnaip2018_SuperHeroes/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CorsoEnaip2018_SuperHeroes.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ CorsoEnaip2018_SuperHeroes/Startup.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Http;
+using System.Text;
+using Microsoft.Extensions.Primitives;
+
+namespace CorsoEnaip2018_SuperHeroes
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            /*
+             * ASP.NET funziona come catena di MIDDLEWARE
+             * 
+             */
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.Use(async (context, next) =>
+            {
+                // prima aggiungo delle logiche
+                context.Response.Headers.Add(
+                    "custom-header",
+                    new StringValues("custom-header-value"));
+
+                // poi chiamo il middleware successivo
+                await next();
+            });
+
+            app.Use(async (context, next) =>
+            {
+                // prima eseguo il middleware successivo
+                await next();
+
+                // poi modifico il risultato del middleware successivo
+                // aggiungendo le logiche di questo middleware
+                if (context.Response.StatusCode == 404)
+                {
+                    await context.Response
+                        .WriteAsync("Pagina non trovataaaaa");
+                }
+            });
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
